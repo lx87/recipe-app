6610a4871ab72a83e12a07f358236386f06c688c
lx87 <limarx87@gmail.com>
2025-05-01 15:03:14 +0300

    v9

--- src/components/shared/CardItem.tsx
+++ src/components/shared/CardItem.tsx
@@ -21,18 +21,21 @@ const CardItem = ({ item, linkPath }: CardItemProps) => {
 
     return (
         <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={item.image} />
+            <Card.Img variant="top" className="p-3 rounded-3" src={item.image} />
+            <hr className="m-0" />
             <Card.Body>
-                <Card.Title>{item.title}</Card.Title>
-                {item.description && (
+                <Card.Title>{item.title.slice(0, 24)}</Card.Title>
+                {item.description ? (
                     <Card.Text>
                         {item.description.slice(0, 60)}
                     </Card.Text>
-                )}
+                ) : <Card.Text>{"#"}{item.id}</Card.Text>}
+            </Card.Body>
+            <Card.Footer className="p-3">
                 <Button variant="primary" onClick={handleNavigate}>
                     View details
                 </Button>
-            </Card.Body>
+            </Card.Footer>
         </Card>
     );
 };
