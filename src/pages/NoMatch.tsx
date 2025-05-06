a64be1443410b547704d14fc4b134f35140256cc
jzxdx <drugokdza@gmail.com>
2025-05-06 23:34:56 +0300

    Update NoMatch.tsx

--- src/pages/NoMatch.tsx
+++ src/pages/NoMatch.tsx
@@ -2,9 +2,13 @@ import { Link } from "react-router-dom";
 
 const NoMatch = () => {
     return (
-        <div>
-            <h1>404</h1>
-            <h2>This page is not exist</h2>
+        <div className="d-flex align-items-center flex-column justify-content-center">
+            <div>
+                <i className="bi bi-4-square me-2 fs-1"></i>
+                <i className="bi bi-0-square me-2 fs-1"></i>
+                <i className="bi bi-4-square fs-1"></i>
+            </div>
+            <h1 className='mt-2'>Not found!</h1>
             <Link to="/">Back homepage</Link>
         </div>
     );
